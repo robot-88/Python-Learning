fb47beb3df9b46b0c03c84d64fd6bad0a155a9d2
robot-88 <aishwarya.rao189@gmail.com>
2024-12-30 01:11:41 +0000

    Python variables and Functions use

---
+++ AddIntegerAssignment.py
@@ -0,0 +1,41 @@
+# add_IntegerNumber= int(input('Please enter an integer'))
+# print(add_IntegerNumber+10);
+
+
+# def hello_world_printer():
+#     print('Hello World')
+
+
+# hello_world_printer()
+
+
+# def name_printer(parameter1):
+#     print(parameter1)  
+
+
+# name = input('Please enter your name')
+
+# name_printer(name) 
+
+
+
+# length = int(input('Please enter the lenght of a Prism'))
+# width = int(input('Please enter the width of a Prism'))
+# height= int(input('Please enter the height of a Prism'))
+
+# def calculate_the_vol_of_Rectangle(l,w,h):
+#     return l*w*h
+
+# print(calculate_the_vol_of_Rectangle(length,width,height))
+
+
+
+Celsius= int(input('Please enter the temperature in Celsius'))
+
+def fahrenheit(C):
+    return 1.8 * C + 32
+
+
+print("The Fahrenheit equivalent of" + str(Celsius) + "degrees Celsius is" + str(fahrenheit(Celsius)))
+
+
